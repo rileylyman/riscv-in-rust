59d19305de3a99191f5ed33fbda306240f5cf9a1
rileylyman <rileylyman@berkeley.edu>
2019-01-04 15:33:42 -0800

    seperate code into diffent files, fix immediate generation

---
+++ src/decode.rs
@@ -0,0 +1,81 @@
+#[allow(overflowing_literals)]
+
+use std::num::ParseIntError;
+
+// Decode an even length hex string into its constituent bytes
+// Code adapted from StackOverflow user `Sven Marnach`
+pub fn decode_hex_to_bytes(hex: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..hex.len()).step_by(2).map(
+        |k| {
+            u8::from_str_radix(&hex[k..k + 2], 16)
+        }
+    ).collect()
+}
+
+pub fn get_opcode(bytes: &[u8]) -> u8 { (bytes[0] << 1) >> 1 }
+
+pub fn get_rd(bytes: &[u8]) -> u8 { ((bytes[1] << 4) >> 3) + (bytes[0] >> 7) }
+
+pub fn get_f3(bytes: &[u8]) -> u8 { (bytes[1] << 1) >> 5 }
+
+pub fn get_rs1(bytes: &[u8]) -> u8 { (bytes[1] >> 7) + ((bytes[2] << 4) >> 3) }
+
+pub fn get_rs2(bytes: &[u8]) -> u8 { ((bytes[3] << 7) >> 3) + (bytes[2] >> 4) }
+
+pub fn get_f7(bytes: &[u8]) -> u8 { bytes[3] >> 1 }
+
+pub fn get_bits(first_byte: u8) -> i32 {
+    match first_byte {
+        b@_ if b & 0x03 <  0x03 => 16, // 16 bit instruction
+        b@_ if b & 0x1F <  0x1F => 32, // 32 bit instruction
+        b@_ if b & 0x3F == 0x1F => 48, // 48 bit instruction
+        b@_ if b & 0x7F == 0x3F => 64, // 64 bit instruction
+        _ => -1, // >= 80 bit instruction
+    }
+}
+
+pub fn decode_sb_immediate(bytes: &[u8]) -> i32 {
+    let mut imm = if bytes[3] >> 7 == 0x1 { 0xFF_FF_E0_00 } else { 0x0 };
+    imm += ((bytes[3] >> 7) as u32) << 12;
+    imm += ((bytes[0] >> 7) as u32) << 11;
+    imm += (((bytes[3] << 1) >> 2) as u32) << 5;
+    imm += (((bytes[1] << 4) >> 4) as u32) << 1;
+
+    imm as i32
+}
+
+pub fn decode_u_type_immediate(bytes: &[u8]) -> i32 {
+    ((((bytes[1] as u32) >> 4) + ((bytes[2] as u32) << 4) + ((bytes[3] as u32) << 12)) << 12) as i32
+}
+
+pub fn decode_uj_type_immediate(bytes: &[u8]) -> i32 {
+    (if bytes[3] >> 7 == 0x1 { 0xFF_E0_00_00 } else { 0x0 }) + 
+        ((((bytes[2] >> 5) << 1) as u32) + (((bytes[3] << 1) as u32) << 3) + 
+        ((((bytes[2] << 3) >> 7) as u32) << 11) + (((bytes[1] >> 4) as u32) << 12) +
+        (((bytes[2] << 4) as u32) << 12) + (((bytes[3] >> 7) as u32) << 20)) as i32
+}
+
+pub fn decode_s_type_immediate(bytes: &[u8]) -> i32 {
+    ((get_f7(bytes) as i32) << 5) + (get_rd(bytes) as i32) + 
+        if bytes[3] >> 7 == 0x1 { 0xFF_FF_F0_00 } else { 0x0 }
+} 
+
+pub fn decode_i_type_immediate(bytes: &[u8]) -> i32 {
+    (((bytes[3] as u32) << 4) + ((bytes[2] >> 4) as u32) +
+        if bytes[3] >> 7 == 0x1 { 0xFF_FF_F0_00 } else { 0x0 }) as i32
+}
+ 
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_decode_sb_immediate() {
+        let predicted_imm = decode_sb_immediate(&[0x63,0x2,0x0,0x0]);
+        println!("Decoded: {:032b}\n Actual: {:032b}", predicted_imm, 4); 
+        assert_eq!(predicted_imm, 4);
+
+        let predicted_imm = decode_sb_immediate(&[0xe3, 0x0e, 0x00, 0xfe]);
+        println!("Decoded: {:032b}\n Actual: {:032b}", predicted_imm, -4); 
+        assert_eq!(predicted_imm, -4);
+    }
+} 
---
+++ src/implement.rs
@@ -0,0 +1,329 @@
+#[allow(overflowing_literals)]
+
+use super::decode::*;
+use super::*;
+
+pub fn handle_r_type(regfile: &mut [u32], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+    
+    let opcode = get_opcode(bytes);
+    let rd     = get_rd(bytes) as usize;
+    let f3     = get_f3(bytes);
+    let rs1    = get_rs1(bytes) as usize;
+    let rs2    = get_rs2(bytes) as usize;
+    let f7     = get_f7(bytes) as u32;
+
+    if opcode == 0x33 && f3 == 0x0 && f7 == 0x0 { // add
+        regfile[rd] = regfile[rs1] + regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x0 && f7 == 0x20 { // sub
+        regfile[rd] = regfile[rs1] - regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x1 && f7 == 0x00 { // sll
+        regfile[rd] = regfile[rs1] << regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x2 && f7 == 0x00 { // slt
+        regfile[rd] = if (regfile[rs1] as i32) < (regfile[rs2] as i32) { 1 } else { 0 }; 
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x3 && f7 == 0x00 { // sltu
+        regfile[rd] = if regfile[rs1] < regfile[rs2] { 1 } else { 0 };
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x4 && f7 == 0x00 { // xor
+        regfile[rd] = regfile[rs1] ^ regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x5 && f7 == 0x00 { // srl
+        regfile[rd] = regfile[rs1] >> regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x5 && f7 == 0x20 { // sra
+        regfile[rd] = ((regfile[rs1] as i32) >> (regfile[rs2] as i32)) as u32;
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x6 && f7 == 0x00 { // or
+        regfile[rd] = regfile[rs1] | regfile[rs2];
+        *pc += 4;
+    }
+    else if opcode == 0x33 && f3 == 0x7 && f7 == 0x00 { // and
+        regfile[rd] = regfile[rs1] & regfile[rs2];
+        *pc += 4;
+    }
+    else {
+        return Err("Invalid R-Type Instruction");
+    }
+    
+    Ok(())
+}
+
+pub fn handle_i_type(regfile: &mut [u32], mem: &mut [u8], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+
+    let opcode = get_opcode(bytes);
+    let rd     = get_rd(bytes) as usize;
+    let f3     = get_f3(bytes);
+    let rs1    = get_rs1(bytes) as usize;
+    let f7     = get_f7(bytes) as u32;
+
+    let immediate = decode_i_type_immediate(bytes);
+
+    if opcode == 0x3 && f3 == 0x0 { //lb
+        let byte: u32 = mem[((regfile[rs1] as i32) + (immediate as i32)) as usize] as u32;
+        regfile[rd] = if byte >> 7 == 0x1 {
+            0xFF_FF_FF_00 + byte
+        } else {
+           byte
+        }; 
+        *pc += 4;
+    }
+    else if opcode == 0x3 && f3 == 0x1 { //lh
+        let bottom = mem[((regfile[rs1] as i32) + (immediate as i32)) as usize] as u32;
+        let top = mem[((regfile[rs1] as i32) + (immediate as i32) + 1) as usize] as u32;
+        let total = bottom + (top << 8);
+        regfile[rd] = if top >> 7 == 0x1 {
+            0xFF_FF_00_00 + total 
+        } else {
+            total
+        };
+        *pc += 4;
+    }
+    else if opcode == 0x3 && f3 == 0x2 { //lw
+        let bottom = mem[((regfile[rs1] as i32) + immediate) as usize] as u32;
+        let low_mid = mem[((regfile[rs1] as i32) + immediate + 1) as usize] as u32;
+        let high_mid = mem[((regfile[rs1] as i32) + immediate + 2) as usize] as u32;
+        let top = mem[((regfile[rs1] as i32) + immediate + 3) as usize] as u32;
+        regfile[rd] = bottom + (low_mid << 8) + (high_mid << 16) + (top << 24);
+        *pc += 4;
+    }
+    else if opcode == 0x3 && f3 == 0x4 { //lbu
+        regfile[rd] = mem[((regfile[rs1] as i32) + immediate) as usize] as u32;
+        *pc += 4;
+    }
+    else if opcode == 0x3 && f3 == 0x5 { //lhu
+        let bottom = mem[((regfile[rs1] as i32) + immediate) as usize] as u32;
+        let top = mem[((regfile[rs1] as i32) + immediate + 1) as usize] as u32;
+        regfile[rd] = bottom + (top << 8);
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x0 { //addi
+        regfile[rd] = ((regfile[rs1] as i32) + immediate) as u32;
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x1 && f7 == 0x0 { //slli
+        regfile[rd] = regfile[rs1] << immediate;
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x2 { //slti
+        regfile[rd] = if (regfile[rs1] as i32) < immediate { 1 } else { 0 };
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x3 { //sltiu
+        regfile[rd] = if regfile[rs1] < (immediate as u32) { 1 } else { 0 };
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x4 { //xori
+        regfile[rd] = regfile[rs1] ^ (immediate as u32);
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x5 && f7 == 0x0 { //srli
+        regfile[rd] = regfile[rs1] >> immediate; 
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x5 && f7 == 0x20 { //srai
+        regfile[rd] = ((regfile[rs1] as i32) >> immediate) as u32;
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x6 { //ori
+        regfile[rd] = regfile[rs1] | (immediate as u32);
+        *pc += 4;
+    }
+    else if opcode == 0x13 && f3 == 0x7 { //andi
+        regfile[rd] = regfile[rs1] & (immediate as u32);
+        *pc += 4;
+    }
+    else if opcode == 0x67 && f3 == 0x0 { // jalr
+        regfile[rd] = *pc + 4;
+        *pc = ((regfile[rs1] as i32) + immediate) as u32;
+    }
+    else if opcode == 0x73 && f3 == 0x0 && f7 == 0x0 { //ecall
+        match regfile[10] {
+            0x1 => {
+                println!("PRINT ECALL: {}", regfile[11]);
+            }
+            0xA => {
+                println!("TERMINATE ECALL");
+                return Err("Program terminated");
+            }
+            _ => {}
+        } 
+    }
+    else if opcode == 0x73 && f3 == 0x0 && f7 == 0x1 { //ebreak
+        return Err("ebreak: unimplemented instruction");
+    }
+    else {
+        return Err("Invalid I-Type Instruction");
+    }
+
+    Ok(())
+}
+
+pub fn handle_s_type(regfile: &mut [u32], mem: &mut [u8], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+    
+    let opcode   = get_opcode(bytes);
+    let f3       = get_f3(bytes);
+    let rs1      = get_rs1(bytes) as usize;
+    let rs2      = get_rs2(bytes) as usize;
+
+    let immediate = decode_s_type_immediate(bytes);
+
+    if opcode == 0x23 && f3 == 0x0 { //sb
+        mem[((regfile[rs1] as i32) + immediate) as usize] = regfile[rs2] as u8;
+
+        *pc += 4;
+    }
+    else if opcode == 0x23 && f3 == 0x1 { //sh
+        let word = regfile[rs2] as u32;
+        let bottom = word as u8;
+        let top = (word >> 8) as u8;
+        mem[((regfile[rs1] as i32) + (immediate as i32)) as usize] = bottom;
+        mem[((regfile[rs1] as i32) + (immediate as i32) + 1) as usize] = top;
+
+        *pc += 4;
+    }
+    else if opcode == 0x23 && f3 == 0x2 { //sw
+        let word = regfile[rs2] as u32;
+        let bottom = word as u8;
+        let low_mid = (word >> 8) as u8;
+        let high_mid = (word >> 16) as u8;
+        let top = (word >> 24) as u8;
+        mem[((regfile[rs1] as i32) + (immediate as i32))     as usize] = bottom;
+        mem[((regfile[rs1] as i32) + (immediate as i32) + 1) as usize] = low_mid;
+        mem[((regfile[rs1] as i32) + (immediate as i32) + 2) as usize] = high_mid;
+        mem[((regfile[rs1] as i32) + (immediate as i32) + 3) as usize] = top;
+
+        *pc += 4;
+    }
+    else { 
+        return Err("Invalid S-Type Instruction");
+    }
+
+    Ok(())
+}
+
+pub fn handle_sb_type(regfile: &mut[u32], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+    let opcode          = get_opcode(bytes);
+    let f3              = get_f3(bytes);
+    let rs1             = get_rs1(bytes) as usize;
+    let rs2             = get_rs2(bytes) as usize;
+
+    let immediate = decode_sb_immediate(bytes);
+
+    if opcode == 0x63 && f3 == 0x0 { // beq
+        if regfile[rs1 as usize] == regfile[rs2 as usize] {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else if opcode == 0x63 && f3 == 0x1 { // bne
+        if regfile[rs1 as usize] != regfile[rs2 as usize] {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else if opcode == 0x63 && f3 == 0x4 { // blt
+        if (regfile[rs1 as usize] as i32) < (regfile[rs2 as usize] as i32) {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else if opcode == 0x63 && f3 == 0x5 { // bge
+        if (regfile[rs1 as usize] as i32) < (regfile[rs2 as usize] as i32) {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else if opcode == 0x63 && f3 == 0x6 { //bltu 
+        if regfile[rs1 as usize] < regfile[rs2 as usize] {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else if opcode == 0x63 && f3 == 0x7 { //bgeu
+        if regfile[rs1 as usize] >= regfile[rs2 as usize] {
+            if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+                return Err("Instruction address misaligned exception");
+            }
+            *pc = ((*pc as i32) + immediate) as u32;
+        } else {
+            *pc += 4;
+        }
+    }
+    else {
+        return Err("Invalid SB-Type Instruction");
+    }
+
+    Ok(())
+}
+
+pub fn handle_u_type(regfile: &mut [u32], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+    let opcode = get_opcode(bytes);
+    let rd     = get_rd(bytes);
+
+    let immediate = decode_u_type_immediate(bytes);
+
+    if opcode == 0x17 { // auipc
+        regfile[rd as usize] = ((*pc as i32) + immediate) as u32;
+        *pc += 4;
+    }
+    else if opcode == 0x37 { // lui
+        regfile[rd as usize] = immediate as u32;
+        *pc += 4;
+    }
+    else { 
+        return Err("Invalid U-Type Instruction");
+    }
+
+    Ok(())
+}
+
+pub fn handle_uj_type(regfile: &mut [u32], bytes: &[u8], pc: &mut u32) -> Result<(), &'static str> {
+    let opcode = get_opcode(bytes);
+    let rd     = get_rd(bytes);
+
+    let immediate = decode_uj_type_immediate(bytes);
+
+    if opcode == 0x6F { //jal 
+        if immediate % INSTRUCTION_ADDRESS_MISALIGNED_THRESHOLD != 0 {
+            return Err("Instruction address misaligned exception");
+        }
+        regfile[rd as usize] = *pc + 4;
+        *pc = ((*pc as i32) + (immediate as i32)) as u32;
+    }
+    else {
+        return Err("Invalid UJ-Type Instruction")
+    }
+
+    Ok(())
+}
